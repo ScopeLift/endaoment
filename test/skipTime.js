9e63c03e4ad95319561a446a003eabf71025fbb6
Ben DiFrancesco <ben@scopelift.co>
2020-02-16 04:56:06 -0700

    Update contract interfaces

--- test/skipTime.js
+++ test/skipTime.js
@@ -5,6 +5,7 @@ contract("Skip Time", accounts => {
     const time1 = await web3.eth.getBlock("latest");
     console.log("start: ", time1.timestamp);
 
+    //const days = .25; //14; // days
     const days = 14; // days
     const duration = days * 24 * 3600;
     await time.increase(duration);
