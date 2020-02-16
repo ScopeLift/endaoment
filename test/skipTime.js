98a0c64c96aafc77672f1a2a1db5b6cd6d5aa950
mds <17163988+mds1@users.noreply.github.com>
2020-02-15 19:04:11 -0700

    Add helpers

---
+++ test/skipTime.js
@@ -0,0 +1,17 @@
+const { time } = require("@openzeppelin/test-helpers");
+
+contract("Skip Time", accounts => {
+  before(async () => {
+    const time1 = await web3.eth.getBlock("latest");
+    console.log("start: ", time1.timestamp);
+
+    const days = 365; // days
+    const duration = days * 24 * 3600;
+    await time.increase(duration);
+
+    const time2 = await web3.eth.getBlock("latest");
+    console.log("start: ", time2.timestamp);
+  });
+
+  it("time skips works", async () => {});
+});
