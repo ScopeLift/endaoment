b566832a523740f34fc64313789b1e91ef458b18
Ben DiFrancesco <ben@scopelift.co>
2020-02-15 18:09:02 -0700

    Unbox and setup Drizzle

---
+++ app/src/MyComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  AccountData,
+  ContractData,
+  ContractForm,
+} from "@drizzle/react-components";
+
+import logo from "./logo.png";
+
+export default ({ accounts }) => (
+  <div className="App">
+    <div>
+      <img src={logo} alt="drizzle-logo" />
+      <h1>Drizzle Examples</h1>
+      <p>Examples of how to get started with Drizzle in various situations.</p>
+    </div>
+
+    <div className="section">
+      <h2>Active Account</h2>
+      <AccountData accountIndex={0} units="ether" precision={3} />
+    </div>
+  </div>
+);
---
+++ app/src/drizzleOptions.js
@@ -0,0 +1,18 @@
+import Web3 from "web3";
+import Moloch from "./contracts/Moloch.json";
+
+const options = {
+  web3: {
+    block: false,
+    customProvider: new Web3("ws://localhost:8545"),
+  },
+  contracts: [Moloch],
+  events: {
+    //SimpleStorage: ["StorageSet"],
+  },
+  polls: {
+    accounts: 1500,
+  },
+};
+
+export default options;
